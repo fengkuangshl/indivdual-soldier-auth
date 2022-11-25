bcfbb000e8fdf900c45147dc72223c2c8be4e360
fengkuangshl <fengkuangshl@163.com>
2022-11-25 14:10:38 +0800

    集成shardingsphere和druid,实现读写分离

---
+++ inner-intergration/shardingsphere-spring-boot-starter/src/main/java/com/key/win/shardingsphere/DruidShardingJdbcDataSourceConfiguration.java
@@ -0,0 +1,24 @@
+package com.key.win.shardingsphere;
+ 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
+import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidFilterConfiguration;
+import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidSpringAopConfiguration;
+import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidStatViewServletConfiguration;
+import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidWebStatFilterConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+ 
+
+@Configuration
+@ConditionalOnClass(DruidDataSourceAutoConfigure.class)
+@EnableConfigurationProperties({DruidStatProperties.class})
+@Import({
+        DruidSpringAopConfiguration.class,
+        DruidStatViewServletConfiguration.class,
+        DruidWebStatFilterConfiguration.class,
+        DruidFilterConfiguration.class})
+public class DruidShardingJdbcDataSourceConfiguration {
+}
